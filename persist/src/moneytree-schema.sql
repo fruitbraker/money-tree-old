aa61ea29559e8a460cf0343d9507951208dd260a
fruitbraker <congpeng89@gmail.com>
2020-02-17 22:12:32 -0600

    Generated persist module files.
    Also created schema to work with for now.

--- persist/src/moneytree-schema.sql
+++ persist/src/moneytree-schema.sql
@@ -1,11 +1,56 @@
+CREATE SCHEMA IF NOT EXISTS mtdev;
+
+SET SCHEMA 'mtdev';
+
 CREATE TABLE IF NOT EXISTS expense_category
 (
-	id VARCHAR(32) PRIMARY KEY
+    expense_category_id VARCHAR(32) PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS expense 
+CREATE TABLE IF NOT EXISTS income_category
 (
-	id SERIAL PRIMARY KEY,
-	test_text VARCHAR(32),
-	category VARCHAR(32) REFERENCES expense_category(id),
+	income_category_id VARCHAR(32) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vendor
+(
+	vendor_id BIGINT PRIMARY KEY NOT NULL,
+	vendor_name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS metadata
+(
+	metadata_id BIGINT PRIMARY KEY NOT NULL,
+	date_created TIMESTAMP DEFAULT now() NOT NULL,
+    date_modified TIMESTAMP DEFAULT now() NOT NULL,
+    notes VARCHAR(64)
+);
+
+CREATE TABLE IF NOT EXISTS expense
+(
+    expense_id BIGINT PRIMARY KEY NOT NULL,
+    transaction_date TIMESTAMP NOT NULL,
+    transaction_amount MONEY NOT NULL,
+    vendor_id BIGINT REFERENCES vendor(vendor_id) NOT NULL,
+    expense_category VARCHAR(32) REFERENCES expense_category(expense_category_id) NOT NULL,
+    metadata BIGINT REFERENCES metadata(metadata_id) NOT NULL,
+    hide boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS budget_target
+(
+	budget_target_id BIGINT PRIMARY KEY NOT NULL,
+	expense_category VARCHAR(32) REFERENCES expense_category(expense_category_id) NOT NULL,
+	amount BIGINT NOT NULL,
+	metadata BIGINT REFERENCES metadata(metadata_id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS income
+(
+	income_id BIGINT PRIMARY KEY NOT NULL,
+	income_source VARCHAR(32) NOT NULL,
+	income_category VARCHAR(32) REFERENCES income_category(income_category_id) NOT NULL,
+    transaction_amount MONEY NOT NULL,
+    transaction_date TIMESTAMP NOT NULL,
+	metadata BIGINT REFERENCES metadata(metadata_id) NOT NULL
 );
