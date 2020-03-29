7e9425fa0ef3b8ec528b5c9d88ff405e2095bf48
fruitbraker <congpeng89@gmail.com>
2020-03-29 16:41:46 -0500

    Added some basic tests to expense routes.

---
+++ mt_api/src/test/kotlin/ExpenseTest.kt
@@ -0,0 +1,52 @@
+import com.moneytree.mtapi.v1.main
+import com.moneytree.mtapi.v1.setUpServer
+import com.natpryce.hamkrest.assertion.assertThat
+import org.http4k.client.OkHttp
+import org.http4k.core.Method.GET
+import org.junit.jupiter.api.Test
+import org.http4k.core.Request
+import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
+import org.http4k.hamkrest.hasStatus
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+class ExpenseTest {
+
+    private val client = OkHttp()
+    private val server = setUpServer()
+    private val url = "http://localhost:9000"
+
+    @BeforeEach
+    fun setup() {
+        server.start()
+    }
+
+    @AfterEach
+    fun teardown() {
+        server.stop()
+    }
+
+    @Test
+    fun `health check happy path`() {
+        assertThat(client(Request(GET, "${url}/health")), hasStatus(OK))
+    }
+
+    @Test
+    fun `OK status returned when retrieving an existing expense by id`() {
+        val id = 1
+        assertThat(client(Request(GET, "${url}/expense/${id}")), hasStatus(OK))
+    }
+
+    @Test
+    fun `NO CONTENT status returned when retrieving expense with nonexistent id`() {
+        val id = Long.MAX_VALUE
+        assertThat(client(Request(GET, "${url}/expense/${id}")), hasStatus(Status.NOT_FOUND))
+    }
+
+    @Test
+    fun `BAD REQUEST status returned when retrieving expense with garbage id`() {
+        val id = "garbage123"
+        assertThat(client(Request(GET, "${url}/expense/${id}")), hasStatus(Status.BAD_REQUEST))
+    }
+}
